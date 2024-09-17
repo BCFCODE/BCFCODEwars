44b386fa0148c11857eec2dc2f3e21cc18414691
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-09-17 17:40:23 +0330

    Google image domain added to next.config.mjs

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,15 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import { auth } from '../../auth';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import { auth } from "../../auth";
+// import Image from "next/image";
 
 export default async function HomePage() {
   const session = await auth();
 
   return (
     <Typography>
-      Welcome to BCFCODEwars, {session?.user?.name || 'User'}!
+      Welcome to BCFCODEwars, {session?.user?.name || "User"}!
+      {/* <Image height={500} width={500} src={session?.user?.image ?? ""} alt="" /> */}
     </Typography>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 
-  /** @type {import('next').NextConfig} */
-  const nextConfig = {};
-  export default nextConfig;
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Add the Google image domain here
+  },
+};
+export default nextConfig;
