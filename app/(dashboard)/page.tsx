660e16ab7f0714258e1507315b8804b36ba7b904
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-09-17 21:27:10 +0330

    Anki Connected!
    AnkiDecks fetched form anki connect and showed in dashboard page!

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import Typography from "@mui/material/Typography";
 import { auth } from "../../auth";
+import AnkiDecks from "./AnkiDecks";
+
 // import Image from "next/image";
 
 export default async function HomePage() {
   const session = await auth();
 
   return (
-    <Typography>
-      Welcome to BCFCODEwars, {session?.user?.name || "User"}!
-      {/* <Image height={500} width={500} src={session?.user?.image ?? ""} alt="" /> */}
-    </Typography>
+    <>
+      <Typography>
+        Welcome to BCFCODEwars, {session?.user?.name || "User"}!
+        {/* <Image height={500} width={500} src={session?.user?.image ?? ""} alt="" /> */}
+      </Typography>
+      <AnkiDecks />
+    </>
   );
 }
