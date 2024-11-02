c8dc0694a3cfe0a522c9d290f46408a41029fb0f
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-11-02 23:58:32 +0330

    🚀 Implement LeaderBoard with 'use client' directive for debugging!
    - Added 'use client' at the top of the LeaderBoard component to enable client-side rendering.
    - Integrated DataGrid from MUI for displaying demo data with a toolbar.
    - Enhanced overall functionality and prepared for improved debugging.

--- app/Leaderboard.tsx
+++ app/Leaderboard.tsx
@@ -1,8 +1,10 @@
+'use client'
+
 import * as React from 'react';
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import { useDemoData } from '@mui/x-data-grid-generator';
 
-export default function ColumnSelectorGrid() {
+export default function LeaderBoard() {
   const { data } = useDemoData({
     dataSet: 'Commodity',
     rowLength: 10,
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LeaderBoard from "./Leaderboard";
+
+const MainPage = () => {
+  return <LeaderBoard />;
+};
+
+export default MainPage;
