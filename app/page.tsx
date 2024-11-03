31e7a93a8c7b345e0f878e4105fa698f3113efe0
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-11-04 00:13:09 +0330

    git commit -m "Add Loading Component with Enhanced UI and SEO Improvements
    - Introduced a Loading component to improve user experience during data fetching.
    - Implemented MUI components for a visually appealing loading screen.
    - Set up styles for responsiveness and theme consistency.
    - Prepared MainPage for future integration with Suspense for loading states.
    - Enhanced accessibility and user feedback with loading messages."

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
 import LeaderBoard from "./Leaderboard";
 
 const MainPage = () => {
-  return <LeaderBoard />;
+  return (
+    <LeaderBoard />
+    // <Suspense fallback={<p>Welcome to BCFCODEwars</p>}>
+    //   <LeaderBoard />
+    // </Suspense>
+  );
 };
 
 export default MainPage;
