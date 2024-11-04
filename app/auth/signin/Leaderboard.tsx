5f9c6812ed533964cd4b2d1d75480388582631e6
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-11-04 23:29:43 +0330

    feat: add Leaderboard component with custom toolbar and SignInButton
    - Created `Leaderboard.tsx` component for displaying data using MUI's DataGrid.
    - Integrated `useDemoData` for mock data display with 10 rows and up to 10 columns.
    - Implemented a custom toolbar with `GridToolbar` and a `SignInButton` for improved user interaction.
    - Styled component for full-width and height responsiveness.

---
+++ app/auth/signin/Leaderboard.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { useDemoData } from "@mui/x-data-grid-generator";
+import SignInButton from "./SignInButton";
+
+const CustomToolbar = () => {
+  const styles = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: "10px",
+  };
+
+  return (
+    <Box {...styles}>
+      <GridToolbar />
+      <SignInButton />
+    </Box>
+  );
+};
+
+export default function LeaderBoard() {
+  const { data } = useDemoData({
+    dataSet: "Employee",
+    rowLength: 10,
+    maxColumns: 10,
+  });
+
+  const styles = {
+    height: "100%",
+    width: "100%",
+  };
+
+  return (
+    <Box {...styles}>
+      <DataGrid
+        {...data}
+        slots={{
+          toolbar: CustomToolbar, // Use the custom toolbar
+        }}
+      />
+    </Box>
+  );
+}
