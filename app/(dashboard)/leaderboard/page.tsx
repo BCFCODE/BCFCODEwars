6181451c7a8658ef31b9332889bb3d589eaff56b
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-11-26 08:55:03 +0330

    Refactor LeaderBoard Component and Update Auth Configuration
    - Simplified the rendering of `<LeaderBoard />` in `page.tsx` by using a single-line return.
    - Removed unnecessary file `Leaderboard.tsx`, improving code structure and reducing complexity.
    - Adjusted `auth.ts` configuration:
      - Commented out the domain setting for `.bcfcode.ir` to ease local development.

--- app/(dashboard)/leaderboard/page.tsx
+++ app/(dashboard)/leaderboard/page.tsx
@@ -1,7 +1,5 @@
-import LeaderBoard from '@/app/Leaderboard';
+import LeaderBoard from "@/app/Leaderboard";
 
 export default function LeaderBoardPage() {
-  return (
-    <LeaderBoard />
-  );
+  return <LeaderBoard />;
 }
