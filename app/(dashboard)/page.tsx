b89976db513915c3b065ceaf339ddbb07efb41a9
Morteza Bakhshandeh <bcfcode@gmail.com>
2024-12-16 15:52:43 +0330

    Debug production base URL differences
    - Updated base URL to use NEXT_PUBLIC_BASE_URL instead of NEXTAUTH_URL for better clarity and alignment with environment variable usage.
    - Cleaned up redundant comments and unused imports for improved readability.
    - Enhanced metadata configuration and image domain setup for consistency across environments.
    - Improved error handling and logging in the authentication flow for a more robust user experience.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,6 @@
 // app/(dashboard)/page.tsx
-import { auth } from "../../auth";
 import UnderDevelopment from "./UnderDevelopment";
 
-
 export default async function HomePage() {
-  const session = await auth(); 
-
   return <UnderDevelopment pageName="Dashboard" />;
 }
