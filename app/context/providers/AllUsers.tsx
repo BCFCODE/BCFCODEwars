cfe56eeb3c300a449ff16779da819c2c6cfbd539
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-02 17:23:45 +0330

    Refactor AllUsers provider to use custom hook for data fetching
    - Removed inline logic for data fetching and state management in AllUsersProvider.
    - Moved all user state handling and session-based fetching logic to a new custom hook `useAllUsers`.
    - Simplified AllUsersProvider by utilizing the `useAllUsers` hook for managing session-based user fetching and state updates.
    - Replaced the `useEffect` and `useReducer` logic inside the provider with the custom hook, improving readability and separation of concerns.
    - Removed redundant session management from the provider and delegated it to the custom hook.
    - Updated AllUsersContext to use the state provided by the `useAllUsers` hook.
    - Enhanced code maintainability and modularity by isolating side-effect and state management in a dedicated hook.
    
    This refactor improves the overall architecture by following best practices for React hooks and context management, making the provider component cleaner and more maintainable.

--- app/context/providers/AllUsers.tsx
+++ app/context/providers/AllUsers.tsx
@@ -1,23 +1,12 @@
 "use client";
 
-import dbAPIService from "@/app/api/services/db";
-import allUsersReducer, {
+import {
   AllUsersAction,
   AllUsersContextType,
-  initialDatabaseAllUsersState,
 } from "@/app/context/reducers/allUsersReducer";
-import { AuthenticatedUser } from "@/types/users";
-import { useSession } from "next-auth/react";
 
-import {
-  createContext,
-  Dispatch,
-  ReactNode,
-  useEffect,
-  useReducer,
-} from "react";
-
-const { getUsers } = new dbAPIService();
+import { createContext, Dispatch, ReactNode } from "react";
+import useAllUsers from "./effects/useAllUsers";
 
 export const AllUsersContext = createContext<AllUsersContextType | null>(null);
 export const AllUsersDispatchContext =
@@ -26,69 +15,10 @@ export const AllUsersDispatchContext =
 interface Props {
   children: ReactNode;
   context?: AllUsersContextType;
-  // currentUserEmail: string;
-  // session: Session | null;
 }
 
 const AllUsersProvider = ({ children }: Props) => {
-  const session = useSession();
-  const [allUsersContext, allUsersDispatch] = useReducer(
-    allUsersReducer,
-    initialDatabaseAllUsersState
-  );
-  
-  useEffect(() => {
-    if (!session.data) return; // Wait for session to load
-    (async () => {
-      try {
-        // Reset loading and error before retrying.
-        allUsersDispatch({ type: "SET_LOADING", loading: true });
-        allUsersDispatch({ type: "SET_ERROR", error: false });
-
-        const fetchedUsers = await getUsers({ cache: "no-store" });
-
-        if (!fetchedUsers.success || fetchedUsers.error) {
-          // console.log("error in AllUsersProvider");
-          allUsersDispatch({ type: "SET_LOADING", loading: false });
-          allUsersDispatch({ type: "SET_ERROR", error: true });
-        }
-
-        if (fetchedUsers.users) {
-          const allUsers = fetchedUsers.users.map((user) => {
-            // console.log(
-            //   "in AllUsersProvider useEffect map",
-            //   user.email,
-            //   session.data
-            // );
-            return user.email === session.data?.user.email
-              ? { ...user, session: session.data }
-              : user;
-          }) as AuthenticatedUser[];
-
-          // const allUsers = fetchedUsers.users as AuthenticatedUser[]
-
-          // console.log("allUsers in AllUsersProvider", allUsers, session.data);
-
-          allUsersDispatch({
-            type: "SET_ALL_USERS",
-            payload: {
-              allUsers,
-              error: false,
-              isLoading: false,
-            },
-          });
-        }
-      } catch (error) {
-        allUsersDispatch({ type: "SET_LOADING", loading: false });
-        allUsersDispatch({ type: "SET_ERROR", error: true });
-        // If an exception occurs, set the error flag.
-        // allUsersDispatch({ type: "SET_ERROR", error: true });
-        // console.error("Error loading leaderboard data");
-      } finally {
-        allUsersDispatch({ type: "SET_LOADING", loading: false });
-      }
-    })();
-  }, []);
+  const { allUsersContext, allUsersDispatch } = useAllUsers();
 
   return (
     <AllUsersContext.Provider value={allUsersContext}>
---
+++ app/context/providers/effects/useAllUsers.ts
@@ -0,0 +1,80 @@
+import dbAPIService from "@/app/api/services/db";
+import allUsersReducer, {
+  AllUsersAction,
+  AllUsersState,
+  initialDatabaseAllUsersState,
+} from "@/app/context/reducers/allUsersReducer";
+import { AuthenticatedUser } from "@/types/users";
+import { useSession } from "next-auth/react";
+import { Dispatch, useEffect, useReducer } from "react";
+
+const { getUsers } = new dbAPIService();
+
+interface UseAllUsers {
+  allUsersContext: AllUsersState;
+  allUsersDispatch: Dispatch<AllUsersAction>;
+}
+
+const useAllUsers = (): UseAllUsers => {
+  const session = useSession();
+  const [allUsersContext, allUsersDispatch] = useReducer(
+    allUsersReducer,
+    initialDatabaseAllUsersState
+  );
+
+  useEffect(() => {
+    if (!session.data) return; // Wait for session to load
+    (async () => {
+      try {
+        // Reset loading and error before retrying.
+        allUsersDispatch({ type: "SET_LOADING", loading: true });
+        allUsersDispatch({ type: "SET_ERROR", error: false });
+
+        const fetchedUsers = await getUsers({ cache: "no-store" });
+
+        if (!fetchedUsers.success || fetchedUsers.error) {
+          // console.log("error in AllUsersProvider");
+          allUsersDispatch({ type: "SET_LOADING", loading: false });
+          allUsersDispatch({ type: "SET_ERROR", error: true });
+        }
+
+        if (fetchedUsers.users) {
+          const allUsers = fetchedUsers.users.map((user) => {
+            // console.log(
+            //   "in AllUsersProvider useEffect map",
+            //   user.email,
+            //   session.data
+            // );
+            return user.email === session.data?.user.email
+              ? { ...user, session: session.data }
+              : user;
+          }) as AuthenticatedUser[];
+
+          // const allUsers = fetchedUsers.users as AuthenticatedUser[]
+
+          // console.log("allUsers in AllUsersProvider", allUsers, session.data);
+
+          allUsersDispatch({
+            type: "SET_ALL_USERS",
+            payload: {
+              allUsers,
+              error: false,
+              isLoading: false,
+            },
+          });
+        }
+      } catch (error) {
+        allUsersDispatch({ type: "SET_LOADING", loading: false });
+        allUsersDispatch({ type: "SET_ERROR", error: true });
+        // If an exception occurs, set the error flag.
+        // allUsersDispatch({ type: "SET_ERROR", error: true });
+        // console.error("Error loading leaderboard data");
+      } finally {
+        allUsersDispatch({ type: "SET_LOADING", loading: false });
+      }
+    })();
+  }, []);
+  return { allUsersContext, allUsersDispatch };
+};
+
+export default useAllUsers;
