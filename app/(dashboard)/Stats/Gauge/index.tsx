c87ed1d3dce85e20d9e906cf1df5b290e58c1b81
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-04-29 20:40:38 +0330

    refactor(stats): unify gauge components into a reusable CodewarsStatGauge
    - Removed separate Daily, Weekly, Monthly, and Yearly stat components
    - Introduced a single reusable <CodewarsStatGauge /> component
    - Dynamically pass completed challenge counts using a custom hook
    - Simplified and cleaned up the Stats page layout
    - Enhanced maintainability and reduced redundancy across the dashboard

--- app/(dashboard)/Stats/Gauge/index.tsx
+++ app/(dashboard)/Stats/Gauge/index.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Stack, Typography } from "@mui/material";
+import { Gauge, gaugeClasses } from "@mui/x-charts/Gauge";
 
-const CodewarsStat = () => {
-  return 
+interface Props {
+  completedChallenges: number;
 }
 
-export default CodewarsStat
+const CodewarsStatGauge = ({ completedChallenges }: Props) => {
+  return (
+    <Stack>
+      <Gauge
+        height={200}
+        value={15}
+        startAngle={-110}
+        endAngle={110}
+        sx={{
+          [`& .${gaugeClasses.valueText}`]: {
+            fontSize: 40,
+            transform: "translate(0px, 0px)",
+          },
+        }}
+        // text={({ value, valueMax }) => `${value} / ${valueMax}`}
+        text={({ value, valueMax }) => `${value}%`}
+      />
+      <Typography sx={{ textAlign: "center" }}>
+        {completedChallenges} in last 365Days
+      </Typography>
+    </Stack>
+  );
+};
+
+export default CodewarsStatGauge;
--- app/(dashboard)/Stats/index.tsx
+++ app/(dashboard)/Stats/index.tsx
@@ -2,22 +2,23 @@
 
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import DailyStat from "./Gauge/Stats/Daily";
-import MonthlyStat from "./Gauge/Stats/Monthly";
-import WeeklyStat from "./Gauge/Stats/Weekly";
-import YearlyStat from "./Gauge/Stats/Yearly";
-
-const stats = [DailyStat, WeeklyStat, MonthlyStat, YearlyStat];
+import CodewarsStatGauge from "./Gauge";
+import useChallengeCountsByPeriod from "./Gauge/hooks/useChallengeCountsByPeriod";
 
 const DashboardStats = () => {
+  const { inLast24Hour, inLast7Days, inLast30Days, inLast365Days } =
+    useChallengeCountsByPeriod();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2} columns={24}>
-        {stats.map((Stat, index) => (
-          <Grid size={{ xs: 24, sm: 12, md: 8, lg: 6, xl: 6 }} key={index}>
-            <Stat />
-          </Grid>
-        ))}
+        {[inLast24Hour, inLast7Days, inLast30Days, inLast365Days].map(
+          (inLastDayAgo, index) => (
+            <Grid size={{ xs: 24, sm: 12, md: 8, lg: 6, xl: 6 }} key={index}>
+              <CodewarsStatGauge completedChallenges={inLastDayAgo} />
+            </Grid>
+          )
+        )}
       </Grid>
     </Box>
   );
