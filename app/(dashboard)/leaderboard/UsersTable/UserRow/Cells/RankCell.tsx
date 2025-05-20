ad579d11578cdeef46f54c4260d12c7dacfb1501
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-05-20 21:19:20 +0330

    feat(rank): display user’s actual Codewars rank in RankCell
    Previously, RankCell displayed a static “N/A” placeholder. This update enhances UX by dynamically showing the user's actual overall Codewars rank.
    
    🔍 Details:
    - Integrated `useCurrentUserContext()` to access the current user.
    - Displayed `currentUser.codewars.ranks.overall.name` if available, with fallback to `N/A`.
    
    This adds meaningful rank visibility to the leaderboard and improves clarity on individual performance.

--- app/(dashboard)/leaderboard/UsersTable/UserRow/Cells/RankCell.tsx
+++ app/(dashboard)/leaderboard/UsersTable/UserRow/Cells/RankCell.tsx
@@ -1,11 +1,14 @@
 import { codewarsCellStyles } from "@/app/(dashboard)/leaderboard/styles";
+import useCurrentUserContext from "@/app/context/hooks/db/useCurrentUserContext";
 import { TableCell } from "@mui/material";
 import React from "react";
 
 const RankCell = () => {
+  const { currentUser } = useCurrentUserContext();
+  
   return (
     <TableCell sx={codewarsCellStyles} align="center">
-      N/A
+      {currentUser.codewars.ranks?.overall.name ?? `N/A`}
     </TableCell>
   );
 };
