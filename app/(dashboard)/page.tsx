d680e10fd18477d929d28a051e78ca6b235aa020
Morteza Bakhshandeh <bcfcode@gmail.com>
2025-07-12 15:18:01 +0330

    feat(dashboard): add DashboardCard components to dashboard page
    Introduced DashboardCard component to enhance the dashboard layout with three customizable card placeholders. Each card features consistent styling with padding, minimum width, border, and rounded corners using MUI's Box component. Updated page.tsx to include these cards in a flexible layout with a gap for improved visual structure.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,12 +3,13 @@ import { Box } from "@mui/material";
 
 import DashboardStats from "./Stats";
 import Welcome from "./Welcome";
+import DashboardCard from "./components/DashboardCard";
 
 export default async function DashboardPage() {
   const session = await auth();
 
   return (
-    <Box>
+    <Box >
       <Welcome
         {...{
           pageName: "Dashboard",
@@ -19,7 +20,41 @@ export default async function DashboardPage() {
           },
         }}
       >
+
         <DashboardStats />
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <DashboardCard
+            sx={{
+              p: 2,
+              minWidth: 300,
+              border: "1px solid",
+              borderColor: "divider",
+              bgcolor: "background.default",
+              borderRadius: 2,
+            }}
+          />
+          <DashboardCard
+            sx={{
+              p: 2,
+              minWidth: 300,
+              border: "1px solid",
+              borderColor: "divider",
+              bgcolor: "background.default",
+              borderRadius: 2,
+            }}
+          />
+          <DashboardCard
+            sx={{
+              p: 2,
+              minWidth: 300,
+              border: "1px solid",
+              borderColor: "divider",
+              bgcolor: "background.default",
+              borderRadius: 2,
+            }}
+          />
+        </Box>
+        
       </Welcome>
     </Box>
   );
